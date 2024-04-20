71df8a5bd36e38e768cb5a0225c0e8618143469a
souviitor <viitorsouza93@gmail.com>
2024-04-19 22:08:06 -0300

    inclusao de js e scrollreveal

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const menu = document.getElementById('burguer');
 const lista = document.getElementById('itens');
 
-function clickMenu (){
+function clickMenu() {
   if (itens.style.display == 'block') {
     itens.style.display = 'none'
-  }else {
+  } else {
     itens.style.display = 'block'
   }
 }
+
+const sr = ScrollReveal({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('.lista', { delay: 200, origin: 'left' });
+sr.reveal('.text', { delay: 200, origin: 'top' });
+sr.reveal('.imagem', { delay: 200, origin: 'top' });
