34db89b2490fe5e59e9ed7fe0a5f802bb2d8ea99
souviitor <viitorsouza93@gmail.com>
2024-04-19 21:00:22 -0300

    inclusao menu hamburguer para mobile

---
+++ main.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('burguer');
+const lista = document.getElementById('itens');
+
+function clickMenu (){
+  if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+  }else {
+    itens.style.display = 'block'
+  }
+}
